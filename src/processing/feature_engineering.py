10c9091b5eda1b8de5482005c8b23ae8ed549992
Charlie Maguire <144113514+CharlieMaguire1@users.noreply.github.com>
2026-03-10 17:27:14 +0000

    Delete src directory

--- src/processing/feature_engineering.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import KNNImputer
-from scipy.stats import zscore
-import seaborn as sns
-import matplotlib.pyplot as plt
-
